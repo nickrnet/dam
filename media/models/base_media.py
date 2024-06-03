9ff43ac97c666f346b122d5e4406083e50d52e7d
Nick Rogers <nrogers@gmail.com>
2024-06-03 17:34:00 -0500

    wip upload a file

--- media/models/base_media.py
+++ media/models/base_media.py
@@ -1,8 +1,31 @@
+import logging
 import uuid
+
+import magic
 
 from django.db import models
 
 
+logger = logging.getLogger(__name__)
+
+
 class BaseMedia(models.Model):
+
+    class Meta:
+        abstract = True
+
+    class AllowedTypes(models.TextChoices):
+        JPG = "image/jpeg", "IMAGE/JPEG"
+        PNG = "image/png", "IMAGE/PNG"
+        GIF = "image/gif", "IMAGE/GIF"
+        MP4 = "image/mp4", "IMAGE/MP4"
+
     file = models.FileField(upload_to=uuid.uuid4(), max_length=256)
     name = models.CharField(max_length=255)
+
+    @classmethod
+    def is_valid_media_type(self, file: bytes) -> bool:
+        mime = magic.Magic(mime=True)
+        mime_type = mime.from_buffer(file)
+        logger.debug(f"Uploaded file mime_type: {mime_type}")
+        return mime_type in self.AllowedTypes.values
--- media/views.py
+++ media/views.py
@@ -2,8 +2,9 @@ from django_htmx.middleware import HtmxDetails
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_POST
 
+from .models.base_media import BaseMedia
 from .models.jpg import Jpg
 
 
@@ -13,5 +14,17 @@ class HtmxHttpRequest(HttpRequest):
 
 @require_GET
 def index(request: HtmxHttpRequest) -> HttpResponse:
+    # TODO: get all media types allowed
     jpgs = Jpg.objects.all()
     return render(request, "index.html", {"jpgs": jpgs})
+
+
+@require_POST
+def upload(request: HtmxHttpRequest) -> HttpResponse:
+    succesful_upload = False
+
+    if BaseMedia.is_valid_media_type(request.FILES["file"].read()):
+        # TODO: save the thing when we know what type it is
+        # Jpg.objects.create(file=request.FILES["file"], name=request.FILES["file"].name)
+        succesful_upload = True
+    return render(request, "upload.html", {"success": succesful_upload})
